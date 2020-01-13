2a8223f17250a69ceb216ef0619581f3e308e58f
Bunnywinkles <bunnywinkles@gmail.com>
2020-01-12 19:20:21 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FlightHours = New System.Windows.Forms.NumericUpDown()
+        Me.FlightMinutes = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.AircraftFuelBurn = New System.Windows.Forms.NumericUpDown()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.AircraftCapacity = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        CType(Me.FlightHours, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FlightMinutes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.AircraftFuelBurn, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.AircraftCapacity, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlightHours
+        '
+        Me.FlightHours.Location = New System.Drawing.Point(9, 32)
+        Me.FlightHours.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
+        Me.FlightHours.Name = "FlightHours"
+        Me.FlightHours.Size = New System.Drawing.Size(120, 20)
+        Me.FlightHours.TabIndex = 0
+        '
+        'FlightMinutes
+        '
+        Me.FlightMinutes.Location = New System.Drawing.Point(146, 32)
+        Me.FlightMinutes.Maximum = New Decimal(New Integer() {60, 0, 0, 0})
+        Me.FlightMinutes.Name = "FlightMinutes"
+        Me.FlightMinutes.Size = New System.Drawing.Size(120, 20)
+        Me.FlightMinutes.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Hours"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(143, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Minutes"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.FlightMinutes)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.FlightHours)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(278, 65)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Flight Information"
+        '
+        'AircraftFuelBurn
+        '
+        Me.AircraftFuelBurn.Location = New System.Drawing.Point(9, 32)
+        Me.AircraftFuelBurn.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.AircraftFuelBurn.Name = "AircraftFuelBurn"
+        Me.AircraftFuelBurn.Size = New System.Drawing.Size(120, 20)
+        Me.AircraftFuelBurn.TabIndex = 5
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.AircraftCapacity)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.AircraftFuelBurn)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 83)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(278, 66)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Aircraft Information"
+        '
+        'AircraftCapacity
+        '
+        Me.AircraftCapacity.Location = New System.Drawing.Point(145, 32)
+        Me.AircraftCapacity.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.AircraftCapacity.Name = "AircraftCapacity"
+        Me.AircraftCapacity.Size = New System.Drawing.Size(120, 20)
+        Me.AircraftCapacity.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(142, 16)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Capacity"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Fuel Burn"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(325, 18)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 7
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lblResult)
+        Me.GroupBox3.Location = New System.Drawing.Point(296, 47)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(129, 102)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Results"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(6, 16)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 159)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Fuel Cost Calculator"
+        CType(Me.FlightHours, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FlightMinutes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.AircraftFuelBurn, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.AircraftCapacity, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlightHours As NumericUpDown
+    Friend WithEvents FlightMinutes As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents AircraftFuelBurn As NumericUpDown
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents AircraftCapacity As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim ACCap As Integer
+        Dim ACFB As Integer
+        Dim FHour As Integer
+        Dim FMinute As Integer
+        Dim Ascent As Integer
+        Dim FuelCost As Double
+        Dim PaxCost As Double
+
+        ACCap = AircraftCapacity.Value
+        ACFB = AircraftFuelBurn.Value
+        FHour = FlightHours.Value
+        FMinute = FlightMinutes.Value
+
+        FMinute = (FHour * 60) + FMinute
+
+        If FMinute < 90 Then
+            Ascent = FMinute / 2
+            FuelCost = ACFB * 10 * Ascent + ACFB * Ascent * 0.08 * 1
+        Else
+            FuelCost = ((ACFB * 10 * 45 + ACFB * (FMinute - 30)) * 0.08 * 1) * (0.7 + 0.3 * 1)
+        End If
+
+        PaxCost = FuelCost / ACCap
+
+        lblResult.Text = "Total Fuel Cost:" & vbCrLf & FormatCurrency(FuelCost, 2) & vbCrLf & vbCrLf & "Cost per Passanger:" & vbCrLf & FormatCurrency(PaxCost, 2)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
